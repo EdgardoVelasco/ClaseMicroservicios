dd78aedf2f703f28d05cd537021cf5b4cf71380b
edgardo.nvf <edgardo.nvf@gmail.com>
2025-02-05 12:05:29 -0500

    clase

--- MicroserviceClient/src/main/java/com/bancolombia/app/entities/Client.java
+++ MicroserviceClient/src/main/java/com/bancolombia/app/entities/Client.java
@@ -1,11 +1,23 @@
 package com.bancolombia.app.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="clients")
 public class Client {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(length=50)
 	private String name;
+	
+	@Column(length=100)
 	private String email;
 	public long getId() {
 		return id;
